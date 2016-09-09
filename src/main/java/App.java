90337187a252720e3a19162eb14e5fa412073c43
Jeremy Fryd <fryd21@gmail.com>
2016-09-09 13:53:02 -0700

    polish App and tests to full functionality

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -30,7 +30,7 @@ public class App {
       System.out.println("'" + name + "', I can dig it! You sure know how to lay the word like a cool sax, and I can respect a cat with a knife of a tongue like that.");
       System.out.println("Now, there's a few joints 'round this town that i think would be some real spots for a ball-roller like that, which one yanks your collar to the floor and 'round:");
       System.out.println();
-      System.out.println("SELECT (THE GREAT ORIENTAL / TIKI TINA'S / THE BOURBON BAYOU / LE STARRY BLEU)");
+      System.out.println("SELECT BY NUMBER (1 TIKI TINA'S [DEFAULT] / 2 THE BOURBON BAYOU / 3 LE STARRY BLEU / 4 THE GREAT ORIENTAL)");
       String location = myConsole.readLine();
       if (location.equals("quit")) {
         jiveness = false;
@@ -41,19 +41,19 @@ public class App {
       System.out.println();
       System.out.println("Ooooh-wee - this is gonna make for a night fine like a ripe grain of beach sand! But, of course, no doghouse is a home without some reeeaal bad tunes. If we want to turn a hot dime, we're gonna have to book the coolest cats from here to St. Louis. Who's gonna drop the blare and lay the jam-clam beat?");
       System.out.println();
-      System.out.println("SELECT (THE BONOBO BOYS / THE SWINGIN JOHNNYS / APALACHEE ERIK AND THE SNOWGLOBIN' HATCHETS / GALWAY GARETH'S PERCUSSION EMPORIUM)");
+      System.out.println("SELECT BY NUMBER (1 THE BONOBO BOYS [DEFAULT] / 2 THE SWINGIN JOHNNYS / 3 APALACHEE ERIK AND THE SNOWGLOBIN' HATCHETS / 4 GALWAY GIL'S PERCUSSION EMPORIUM)");
       String band = myConsole.readLine();
       if (band.equals("quit")) {
         jiveness = false;
       }
       theBigTime.tuneLayinDaddies(band);
 
-      String cost = theBigTime.theBigBusinessJive();
+      theBigTime.theBigBusinessJive();
 
       System.out.println();
       System.out.println("Cut me some shampoo and call me Sampson - I think we're cullin' some of grandma's sweet tutti-frutti over here! This riot is gonna cut the lights outta this town - no doubts or dispatches - and you and me are gonna rake in the business like a couple of real good-time charlies. The " + name + " is gonna turn " + location + " into a real groovy slide-time, and I can already feel " + band + " reelin' in the fish all night.");
       System.out.println();
-      System.out.println("Now, a ball like this can't roll without catchin' a pretty penny. This grand-slam-momma-jam is gonna run us about " + cost + " deep, but don't let that hold 'ya down. By the end of that night, you and me are gonna me a couple real Carnegies and won't nobody be callin' us a pair of slipaway jacksons then! We'll sure slam 'em then!");
+      System.out.println("Now, a ball like this can't roll without catchin' a pretty penny. This grand-slam-momma-jam is gonna run us about " + theBigTime.mTotalCost + " large, but don't let that hold 'ya down. By the end of that night, you and me are gonna me a couple real Carnegies and won't nobody be callin' us a pair of slipaway jacksons then! We'll sure slam 'em then!");
       System.out.println();
       System.out.println("CONGRATULATIONS, YOU HAVE COMPLETED the big jazz. THANK YOU FOR PLAYING.");
       jiveness = false;
--- src/main/java/TheBigJazz.java
+++ src/main/java/TheBigJazz.java
@@ -4,7 +4,10 @@
 public class TheBigJazz {
   public String mName;
   public String mLocation;
+  public int mLocationCost;
   public String mBand;
+  public int mBandCost;
+  public int mTotalCost;
   public String[] yesArray = {"Y", "y","YES", "yes","yeah", "yee-haw"};
   public String yes = String.join(" ", yesArray);
 
@@ -23,16 +26,40 @@ public class TheBigJazz {
   }
 
   public void theHotSpot(String _location) {
-    mLocation = _location;
+    if (_location.equals("4")) {
+      mLocation = "THE GREAT ORIENTAL";
+      mLocationCost = 25;
+    } else if (_location.equals("3")) {
+      mLocation = "LE STARRY BLEU";
+      mLocationCost = 10;
+    } else if (_location.equals("2")) {
+      mLocation = "THE BOURBON BAYOU";
+      mLocationCost = 4;
+    } else {
+      mLocation = "TIKI TINA'S";
+      mLocationCost = 2;
+    }
   }
+
 
   public void tuneLayinDaddies(String _band) {
-    mBand = _band;
+    if (_band.equals("4")) {
+      mBand = "GALWAY GIL'S PERCUSSION EMPORIUM";
+      mBandCost = 25;
+    } else if (_band.equals("3")) {
+      mBand = "APALACHEE ERIK AND THE SNOWGLOBIN' HATCHETS";
+      mBandCost = 10;
+    } else if (_band.equals("2")) {
+      mBand = "THE SWINGIN JOHNNYS";
+      mBandCost = 4;
+    } else {
+      mBand = "THE BONOBO BOYS";
+      mBandCost = 2;
+    }
   }
 
-  public String theBigBusinessJive() {
-    String cost = mName + mLocation + mBand;
-    return cost;
+  public void theBigBusinessJive() {
+    mTotalCost = mLocationCost + mBandCost;
   }
 
 }
